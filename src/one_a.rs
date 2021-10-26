09d07c0517e53c42ba8269da9bf9dfdb6026a34d
Philippe Gaultier <philippe.gaultier@ppro.com>
2021-10-26 14:33:30 +0200

    itertools

--- src/one_a.rs
+++ src/one_a.rs
@@ -1,6 +1,8 @@
 use std::fs::File;
 use std::io::Read;
 
+use itertools::Itertools;
+
 pub fn solve() {
     let mut f = File::open("1a.txt").unwrap();
     let mut content = String::new();
@@ -12,11 +14,13 @@ pub fn solve() {
         .map(|l| l.parse::<usize>().unwrap())
         .collect::<Vec<_>>();
 
-    for (i, a) in numbers.iter().enumerate() {
-        for b in &numbers[0..i] {
-            if a + b == 2020 {
-                println!("{}", a * b);
-            }
-        }
-    }
+    println!(
+        "{}",
+        numbers
+            .iter()
+            .cartesian_product(&numbers)
+            .find(|(a, b)| *a + *b == 2020)
+            .map(|(a, b)| a * b)
+            .unwrap()
+    );
 }
